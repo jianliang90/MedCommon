84aec256c88ddece9727ea144455a30a34b9749a
searobbersduck <searobbersandduck@gmail.com>
2020-11-20 11:32:47 +0800

    commit

--- utils/metrics_utils.py
+++ utils/metrics_utils.py
@@ -17,10 +17,14 @@ class MetricsUtils:
         pass
 
     @staticmethod
-    def dice_coef(y_true, y_pred, smooth=1):
-        intersection = K.sum(y_true * y_pred, axis=[1,2,3])
-        union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3])
-        dice = K.mean((2. * intersection + smooth)/(union + smooth), axis=0)
+    def dice_coef(gt_arr, pred_arr, smooth=1):
+        gt_arr[gt_arr>0] = 1
+        pred_arr[pred_arr>0] = 1
+    
+        intersect = (gt_arr*pred_arr).sum()
+        denominator = gt_arr.sum() + pred_arr.sum()
+        smooth = 1e-8
+        dice = 2 * ((intersect + smooth) / (denominator + smooth))
         return dice
 
     def eval_dice(pred_mask_file, gt_mask_file):
