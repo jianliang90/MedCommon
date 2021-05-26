7283e18379d203ccec2305211139d20909e16082
searobbersduck <searobbersandduck@gmail.com>
2021-05-26 11:08:34 +0800

    commit

--- detection/datasets/position_detection_common_ds.py
+++ detection/datasets/position_detection_common_ds.py
@@ -18,14 +18,36 @@ from torch.utils.data import Dataset, DataLoader
 import numpy as np
 from tqdm import tqdm
 
+import json
+
+def extract_boundary_info(mask_root, out_file):
+    os.makedirs(os.path.dirname(out_file), exist_ok=True)
+    info_dict = {}
+    for filename in tqdm(os.listdir(mask_root)):
+        mask_file = os.path.join(mask_root, filename)
+        boundary_info = MaskBoundingUtils.extract_mask_file_bounding(mask_file)
+        image = sitk.ReadImage(mask_file)
+        image_shape = image.GetSize()
+        info = list(boundary_info) + list(image_shape)
+        info_dict[filename] = [int(i) for i in info]
+    with open(out_file, 'w') as f:
+        f.write(json.dumps(info_dict))
+    print('====> extract_boundary_info finished!')
+    
+
+
 class PositionDetectionDS(Dataset):
-    def __init__(self, root, image_shape=[128,128,128]) -> None:
+    def __init__(self, root, image_shape=[128,128,128], boundary_info_file=None) -> None:
         super().__init__()
         self.root = root
         self.image_root = os.path.join(self.root, 'images')
         self.mask_root = os.path.join(self.root, 'masks')
         self.image_files = []
         self.targets = []
+        boundary_infos = None
+        if boundary_info_file:
+            with open(boundary_info_file) as f:
+                boundary_infos = json.loads(f.read())
         for filename in tqdm(os.listdir(self.image_root)):
             image_file = os.path.join(self.image_root, filename)
             mask_file = os.path.join(self.mask_root, filename)
@@ -33,12 +55,24 @@ class PositionDetectionDS(Dataset):
                 continue
             if not os.path.exists(mask_file):
                 continue
-            z_min, y_min, x_min, z_max, y_max, x_max = MaskBoundingUtils.extract_mask_file_bounding(mask_file)
+            if boundary_info_file:
+                z_min, y_min, x_min, z_max, y_max, x_max = boundary_infos[filename][:6]
+                in_shape = boundary_infos[filename][6:]
+            else:
+                z_min, y_min, x_min, z_max, y_max, x_max = MaskBoundingUtils.extract_mask_file_bounding(mask_file)
+                in_image = sitk.ReadImage(image_file)
+                in_shape = in_image.GetSize()
             self.image_files.append(image_file)
-            in_image = sitk.ReadImage(image_file)
-            in_shape = in_image.GetSize()
+
             x_min, y_min, z_min = DETECTION_UTILS.point_coordinate_resampled(in_shape, image_shape, [x_min, y_min, z_min])
             x_max, y_max, z_max = DETECTION_UTILS.point_coordinate_resampled(in_shape, image_shape, [x_max, y_max, z_max])
+            # 归一化
+            x_min /= image_shape[0]
+            x_max /= image_shape[0]
+            y_min /= image_shape[1]
+            y_max /= image_shape[1]
+            z_min /= image_shape[2]
+            z_max /= image_shape[2]
             self.targets.append(np.array([[z_min, y_min, x_min, z_max, y_max, x_max]]))
             # if self.image_files.__len__() > 2:
             #     break
@@ -56,7 +90,8 @@ class PositionDetectionDS(Dataset):
 
 def test_PositionDetectionDS():
     root = '/data/medical/brain/cerebral_parenchyma/exp/cta'
-    ds = PositionDetectionDS(root)
+    boundary_info_file='/data/medical/brain/cerebral_parenchyma/exp/cta/config/mask_boundary_info.json'
+    ds = PositionDetectionDS(root, boundary_info_file=boundary_info_file)
 
     dataloader = DataLoader(ds, batch_size=1)
 
@@ -66,5 +101,7 @@ def test_PositionDetectionDS():
 
 
 if __name__ == '__main__':
-    test_PositionDetectionDS()
+    # extract_boundary_info(mask_root='/data/medical/brain/cerebral_parenchyma/exp/cta/masks', out_file='/data/medical/brain/cerebral_parenchyma/exp/cta/config/mask_boundary_info.json')
+    extract_boundary_info(mask_root='/data/medical/cardiac/seg/coronary/coronary_ori/masks', out_file='/data/medical/cardiac/seg/coronary/coronary_ori/config/mask_boundary_info.json')
+    # test_PositionDetectionDS()
 
--- detection/runner/regression_detection_trainner.py
+++ detection/runner/regression_detection_trainner.py
@@ -9,12 +9,37 @@ sys.path.append(os.path.join(MEDCOMMON_ROOT, 'external_lib'))
 
 from tqdm import tqdm
 import time
+import numpy as np
 
 from utils.misc_utils import AverageMeter
 from detection.models.detection_auto_resample import RegressionDetecter
 from detection.datasets.position_detection_common_ds import PositionDetectionDS
 from utils.detection_utils import PYTORCH_TENSOR_DETECTION_UTILS
 from utils.lr_adjust_utils import LR_ADJUST_UTILS
+from detection.models.detection_auto_resample import RegressionDetecter
+from detection.datasets.position_detection_common_ds import PositionDetectionDS
+from utils.detection_utils import PYTORCH_TENSOR_DETECTION_UTILS
+from torch.utils.data import Dataset, DataLoader
+
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='common detection algorithm')
+    parser.add_argument('--dataroot', type=str, default='/data/medical/brain/cerebral_parenchyma/exp/cta', help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
+    parser.add_argument('--name', type=str, default='experiment_name', help='name of the experiment. It decides where to store samples and models')
+    parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
+    parser.add_argument('--weights', type=str, default=None, help='pretrained weights file')
+
+    parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate for adam')
+    parser.add_argument('--boundary_info_file', type=str, default='/data/medical/brain/cerebral_parenchyma/exp/cta/config/mask_boundary_info.json', help='file to record mask boundary information')
+    parser.add_argument('--batch_size', type=int, default=1)
+
+    parser.add_argument('--n_objects', type=int, default=1, help='n objects to be detected')
+    parser.add_argument('--input_shape', nargs='+', type=int, default=[128,128,128])
+    parser.add_argument('--aug', default='inference')
+    parser.add_argument('--arch', default='resnet18')
+    parser.add_argument('--n_epochs', type=int, default=100)
+    return parser.parse_args()
 
 class RegressionDetectionTrainer:
     def __init__(self) -> None:
@@ -31,6 +56,7 @@ class RegressionDetectionTrainer:
         losses = AverageMeter()
         end = time.time()
         logger = []
+        ious = np.array([], dtype=np.float)
         for num_iter, (images, targets, names) in tqdm(enumerate(dataloader)):
             data_time.update(time.time() - end)
             output = model(images.cuda())
@@ -40,6 +66,7 @@ class RegressionDetectionTrainer:
                 iou,_ = PYTORCH_TENSOR_DETECTION_UTILS.calc_brick_iou(output, targets.view(targets.shape[0],-1).cuda())
                 l1_loss = torch.nn.L1Loss()(output, targets.view(targets.shape[0],-1).cuda())
                 loss = l1_loss + (1-iou.mean())
+                ious = np.append(ious, iou.detach().cpu())
             if phase == 'train':
                 optimizer.zero_grad()
                 loss.backward()
@@ -54,17 +81,50 @@ class RegressionDetectionTrainer:
                     print(print_info)
                     # print(losses.sum, '\t', losses.count)
                     # print(loss.detach().cpu().numpy())
-                    logger.append(print_info)                                         
+                    logger.append(print_info)
+        return ious                                         
+
+def train():
+    opts = parse_args()
+    root = opts.dataroot
+    boundary_info_file = opts.boundary_info_file
+    ds = PositionDetectionDS(root, boundary_info_file=boundary_info_file)
+    dataloader = DataLoader(ds, batch_size=opts.batch_size)
+
+    n_objects = opts.n_objects
+    net_args = {
+        'input_size': [128, 128, 128], 
+        'arch':opts.arch
+    }
+    model = RegressionDetecter(n_objects, net_args)
+
+    criterion = None
+    lr = opts.lr
+    optimizer = torch.optim.Adam(model.parameters(), lr=opts.lr)
+
+    n_epochs = opts.n_epochs
+    best_iou = 0
+    for epoch in range(n_epochs):
+        LR_ADJUST_UTILS.adjust_learning_rate(optimizer, epoch, opts.lr, True, n_epochs)
+        ious = RegressionDetectionTrainer.train_one_epoch(dataloader, model.cuda(), criterion, optimizer, epoch, 1, phase='train', opt=None)
+        if ious.mean() > best_iou:
+            best_iou = ious.mean()
+            print('current best train iou is:\t{}'.format(best_iou))
+            model_dir = os.path.join(opts.checkpoints_dir, opts.name)
+            os.makedirs(model_dir, exist_ok=True)                
+            saved_model_path = os.path.join(model_dir, 'common_det_epoch_{}_train_{:.3f}'.format(epoch, best_iou))
+            torch.save(model.state_dict(), saved_model_path)
+            print('====> save model:\t{}'.format(saved_model_path))
+
+        
+    print('hello world!')
+    
 
 
 def test_RegressionDetectionTrainer():
-    from detection.models.detection_auto_resample import RegressionDetecter
-    from detection.datasets.position_detection_common_ds import PositionDetectionDS
-    from utils.detection_utils import PYTORCH_TENSOR_DETECTION_UTILS
-    from torch.utils.data import Dataset, DataLoader
-    
     root = '/data/medical/brain/cerebral_parenchyma/exp/cta'
-    ds = PositionDetectionDS(root)
+    boundary_info_file='/data/medical/brain/cerebral_parenchyma/exp/cta/config/mask_boundary_info.json'
+    ds = PositionDetectionDS(root, boundary_info_file=boundary_info_file)
     dataloader = DataLoader(ds, batch_size=1)
     model = RegressionDetecter(1)
 
@@ -73,10 +133,17 @@ def test_RegressionDetectionTrainer():
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     n_epochs = 100
+    best_iou = 0
     for epoch in range(n_epochs):
         LR_ADJUST_UTILS.adjust_learning_rate(optimizer, epoch, lr, True, n_epochs)
-        RegressionDetectionTrainer.train_one_epoch(dataloader, model.cuda(), criterion, optimizer, epoch, 1, phase='train', opt=None)
+        ious = RegressionDetectionTrainer.train_one_epoch(dataloader, model.cuda(), criterion, optimizer, epoch, 1, phase='train', opt=None)
+        if ious.mean() > best_iou:
+            best_iou = ious.mean()
+            print('current best train iou is:\t{}'.format(best_iou))
+            model_dir = os.path.join(opts.checkpoints_dir, opts.name)
+
 
 
 if __name__ == '__main__':
-    test_RegressionDetectionTrainer()
+    # test_RegressionDetectionTrainer()
+    train()
