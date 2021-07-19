534dd6e04e27d4f924ff52aa6e5072560a95ccc8
searobbersduck <searobbersandduck@gmail.com>
2021-07-19 15:42:58 +0800

    commit

--- utils/image_show_utils.py
+++ utils/image_show_utils.py
@@ -3,6 +3,12 @@ import sys
 import numpy as np
 import cv2
 
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+sys.path.append(ROOT)
+
+from utils.datasets_utils import DatasetsUtils
+import SimpleITK as sitk
+
 class ImageShowUtils:
     def __init__(self):
         pass
@@ -20,6 +26,7 @@ class ImageShowUtils:
             out_arr = out_arr*255
             cv2.imwrite(out_file, out_arr)
 
+    @staticmethod
     def save_volume_to_jpg(in_arr, out_root, ww, wl, axis=0, file_prefix=None, reverse=False, lut_name=None):
         '''
         in_arr = sitk.GetArrayFromImage(sitk_image)
@@ -50,11 +57,35 @@ class ImageShowUtils:
                 tmp_arr = tmp_arr[::-1,:]
             ImageShowUtils.save_img(tmp_arr, sub_file_name, ww, wl, lut)
 
+    @staticmethod
+    def save_volume_to_mpr_jpg(in_image, out_dir, ww=150, wl=50, prefix='xxx'):
+        resampled_img = DatasetsUtils.resample_unified_spacing_x_default_min(in_image)
+        arr = sitk.GetArrayFromImage(resampled_img)
+        
+        [z,y,x] = arr.shape
+        z_plane = arr[z//2, :, :]
+        y_plane = arr[:, y//2, :]
+        x_plane = arr[:,:,x//2]
+
+        os.makedirs(out_dir, exist_ok=True)
+        z_file = os.path.join(out_dir, '{}_z.jpg'.format(prefix))
+        y_file = os.path.join(out_dir, '{}_y.jpg'.format(prefix))
+        x_file = os.path.join(out_dir, '{}_x.jpg'.format(prefix))
+
+        cv2.imwrite(z_file, z_plane)
+        cv2.imwrite(y_file, y_plane)
+        cv2.imwrite(x_file, x_plane)
 
 
 def test_save_volume_to_jpg():
     print('todo')
 
+def test_save_volume_to_mpr_jpg():
+    infile = '/data/medical/brain/gan/cta2dwi_history_pos/5.train_batch/1014186/fixed_cta.nii.gz'
+    image = sitk.ReadImage(infile)
+    ImageShowUtils.save_volume_to_mpr_jpg(image, '/data/medical/tmp/mpr')
+
 if __name__ == '__main__':
-    test_save_volume_to_jpg()
+    # test_save_volume_to_jpg()
+    test_save_volume_to_mpr_jpg()
 
